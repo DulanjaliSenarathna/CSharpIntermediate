00a72c0fdb93bd9d4e46364b47be62476e947d47
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-27 12:27:50 +0530

    Introduction Classes

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -10,13 +10,21 @@ namespace CSharpIntermediate
         {
             Console.WriteLine("Hi {0}, I'm {1}", to ,Name);
         }
+
+        //create person object from a string.
+        public static Person Parse(string str)//returns the person object. that's why method return type is Person
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.Name = "Nisitha";
+            var person = Person.Parse("Nisitha");
             person.Introduce("Dulanjali");
         }
     }
