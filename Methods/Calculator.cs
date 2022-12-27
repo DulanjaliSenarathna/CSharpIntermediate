09ec593a26ad9cdd69cfff3591493973a3f2837d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-27 20:36:48 +0530

    Fields

--- Methods/Calculator.cs
+++ Methods/Calculator.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Methods
 {
-    class Calculator
+    public class Calculator
     {
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
     }
 }
