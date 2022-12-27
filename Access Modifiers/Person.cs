62405b0d3d4d5288f5e582bcc0d74b8326247312
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-27 22:24:08 +0530

    Access Modifiers

---
+++ Access Modifiers/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Access_Modifiers
+{
+  public  class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            this._birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+
+    }
+}
---
+++ Access Modifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Access_Modifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
