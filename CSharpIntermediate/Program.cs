2bd8194a56ec819b63cda8bc2597af8686292b0c
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-27 12:06:56 +0530

    Initial commit

---
+++ CSharpIntermediate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I'm {1}", to ,Name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Nisitha";
+            person.Introduce("Dulanjali");
+        }
+    }
+}
