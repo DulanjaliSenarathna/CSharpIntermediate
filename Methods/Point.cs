382272334a0bc91376d62da07116c039c4e91e91
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-27 19:35:32 +0530

    Methods

---
+++ Methods/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            //if newLocation == null throwing an error
+            if (newLocation == null)
+                throw new ArgumentNullException("newLocation");
+            //this.X = newLocation.X;
+            //this.Y = newLocation.Y; instead of this 2 lines redundent, we can call Move method here
+            Move(newLocation.X, newLocation.Y);
+
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //without tryparse (with parse)
+            try
+            {
+                var num = int.Parse("abc");
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Conversion failed..");
+            }
+            //out modifier example
+            int number;
+            var result = int.TryParse("abc", out number);
+            if(result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion faild");
+        }
+
+        static void UseParams()
+        {
+            var cal = new Calculator();
+            Console.WriteLine(cal.Add(1, 2));
+            Console.WriteLine(cal.Add(1, 2, 3));
+            Console.WriteLine(cal.Add(1, 2, 3, 4));
+            Console.WriteLine(cal.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        static void UsePoint()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(new Point(30, 40));
+                Console.WriteLine("Point is at {0} , {1}", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at {0} , {1}", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("An unexpected error occured");
+            }
+        }
+    }
+}
