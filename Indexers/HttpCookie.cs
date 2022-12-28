70547e88967c88a04aa5cff253545f59db3e6732
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-28 11:39:40 +0530

    Indexers

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indexers
+{
+    class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+        public DateTime Expiry { get; set; }
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        //without indexer
+        public void SetCookie(string key, string value)
+        {
+
+        }
+
+        public string GetCookie(string key)
+        {
+            return key;
+        }
+
+        //with indexer
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value }
+        }
+    }
+}
--- Properties/Program.cs
+++ Properties/Program.cs
@@ -7,9 +7,8 @@ namespace Properties
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.Birthdate = new DateTime(1993, 9, 29);
-            Console.WriteLine(person.Age);
+            var person = new Person(new DateTime(1993, 9, 29));
+            Console.WriteLine(person.Age); 
         }
     }
 }
