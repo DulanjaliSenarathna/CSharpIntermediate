c17789aaf736476ccb8434a8b4a8d2cf80cf64df
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-28 13:53:29 +0530

    Inheritance

--- Indexers/Program.cs
+++ Indexers/Program.cs
@@ -6,7 +6,9 @@ namespace Indexers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cookie = new HttpCookie();
+            cookie["name"] = "Dul";
+            Console.WriteLine(cookie["name"]); //output Dul
         }
     }
 }
---
+++ Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inheritance
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;//attribute from parent cls
+            text.Copy();//method from parent cls
+
+        }
+    }
+}
