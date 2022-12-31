fc1889d7dc8e99e6fd586ad73aa5f9c243843cc6
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-31 11:19:45 +0530

    Extensibility

---
+++ Extensibility/DbMigrator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Extensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+
+        public DbMigrator(ILogger logger)//inject the interface to constructor(Dependancy injection)
+        {
+            this._logger = logger;
+        }
+        public void Migrator()
+        {
+            _logger.LogInfo("Migration started at " + DateTime.Now);
+            _logger.LogInfo("Migration finished at " + DateTime.Now);
+           
+        }
+    }
+}
---
+++ Extensibility/ILogger.cs
@@ -0,0 +1,9 @@
+﻿namespace Extensibility
+{
+    public interface ILogger
+    {
+        void LogError(string message);
+        void LogInfo(string message);
+
+    }
+}
